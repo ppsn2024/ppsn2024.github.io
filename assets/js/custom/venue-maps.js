37f044eb388fab6d0704864e11b64a5f6a6c98d0
chrishaider <>
2023-10-13 14:24:44 +0200

    Updated venue and call pages
    - Updated layouts of venue and call page
    - Fixed js includes based on sites
    - Removed unused slider js

--- assets/js/custom/venue-maps.js
+++ assets/js/custom/venue-maps.js
@@ -1,8 +1,34 @@
 (function (L) {
+    var map = L.map('map-parkhotel', {
+        dragging: !L.Browser.mobile,
+        tap: !L.Browser.mobile
+    }).setView([48.3707057, 14.5163663], 7);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        scrollWheelZoom: false,
+        accessToken: 'pk.eyJ1IjoianplbmlzZWsiLCJhIjoiY2wyeXIxNWVpMDJvMjNjcWJpYWloZ29vdSJ9.aVp5R2VZRdkWBrCdo8J90Q'
+    }).addTo(map);
+    map.scrollWheelZoom.disable();
+
+
+    var marker = L.marker([48.3707057, 14.5163663]).addTo(map)
+        .bindPopup('Parkhotel Hagenberg<br/>Hauptstraße 103<br/>4232 Hagenberg im Mühlkreis'
+            + '<br/><br/><a target="_blank" href="https://www.openstreetmap.org/directions?from=&to=48.37058%2C14.51619">Route here (via OpenStreetMap)</a>'
+            + '<br/><a target="_blank" href="https://www.google.at/maps/dir//Hauptstra%C3%9Fe+103,+Hagenberg+im+M%C3%BChlkreis/@48.3706298,14.5133277,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x4773a69bd5596e3b:0xcae56a346af5deb3!2m2!1d14.5159026!2d48.3706263?entry=ttu">Route here (via Google Maps)</a>');
+
+    marker.openPopup();
+})(L);
+
+(function (L) {
     var map = L.map('map', {
         dragging: !L.Browser.mobile,
         tap: !L.Browser.mobile
-    }).setView([48.36936535443323, 14.513704089483294], 5);
+    }).setView([48.36936535443323, 14.513704089483294], 7);
 
     // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
