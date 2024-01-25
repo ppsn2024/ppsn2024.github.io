21307843e90595dab343e97cfab8ee57a01537ae
Jan Zenisek <janzenisek@gmx.at>
2024-01-25 10:51:50 +0100

    updated venue information

--- assets/js/custom/venue-maps.js
+++ assets/js/custom/venue-maps.js
@@ -1,4 +1,56 @@
 (function (L) {
+    var map = L.map('map-aec', {
+        dragging: !L.Browser.mobile,
+        tap: !L.Browser.mobile
+    }).setView([48.3707057, 14.5163663], 7);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        scrollWheelZoom: false,
+        accessToken: 'pk.eyJ1IjoianplbmlzZWsiLCJhIjoiY2wyeXIxNWVpMDJvMjNjcWJpYWloZ29vdSJ9.aVp5R2VZRdkWBrCdo8J90Q'
+    }).addTo(map);
+    map.scrollWheelZoom.disable();
+
+
+    var marker = L.marker([48.3707057, 14.5163663]).addTo(map)
+        .bindPopup('Ars Electronica Center<br/>Ars-Electronica-Straße 1<br/>4040 Linz'
+            + '<br/><br/><a target="_blank" href="https://www.openstreetmap.org/directions?from=&to=48.30963%2C14.28445">Route here (via OpenStreetMap)</a>'
+            + '<br/><a target="_blank" href="https://www.google.at/maps/dir//Ars-Electronica-Stra%C3%9Fe+1,+Linz/@48.3102683,14.2440466,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x47739780646f8959:0xdbdd14faa479667e!2m2!1d14.285246!2d48.3102186?entry=ttu">Route here (via Google Maps)</a>');
+
+    marker.openPopup();
+})(L);
+
+(function (L) {
+    var map = L.map('map-freistadt', {
+        dragging: !L.Browser.mobile,
+        tap: !L.Browser.mobile
+    }).setView([48.3707057, 14.5163663], 7);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        scrollWheelZoom: false,
+        accessToken: 'pk.eyJ1IjoianplbmlzZWsiLCJhIjoiY2wyeXIxNWVpMDJvMjNjcWJpYWloZ29vdSJ9.aVp5R2VZRdkWBrCdo8J90Q'
+    }).addTo(map);
+    map.scrollWheelZoom.disable();
+
+
+    var marker = L.marker([48.3707057, 14.5163663]).addTo(map)
+        .bindPopup('Freistädter Brauhaus<br/>Brauhausstraße 2<br/>4240 Freistadt'
+            + '<br/><br/><a target="_blank" href="https://www.openstreetmap.org/directions?from=&to=48.51011%2C14.50133">Route here (via OpenStreetMap)</a>'
+            + '<br/><a target="_blank" href="https://www.google.at/maps/dir//Freist%C3%A4dter+Brauhaus,+Brauhausstra%C3%9Fe+2,+4240+Freistadt/@48.5101945,14.4601395,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x47730b9f01f28049:0xbd15441ec3bc8d57!2m2!1d14.5013711!2d48.5101433!3e0?entry=ttu">Route here (via Google Maps)</a>');
+
+    marker.openPopup();
+})(L);
+
+(function (L) {
     var map = L.map('map-parkhotel', {
         dragging: !L.Browser.mobile,
         tap: !L.Browser.mobile
---
+++ assets/js/custom/venue-sliders.js
@@ -0,0 +1,27 @@
+var slider = new IdealImageSlider.Slider({
+    selector: '#slider',
+    height: 400, // Required but can be set by CSS
+    interval: 5000
+});
+slider.start();
+
+var parkhotel_slider = new IdealImageSlider.Slider({
+    selector: '#parkhotel_slider',
+    height: 400, // Required but can be set by CSS
+    interval: 5000
+});
+parkhotel_slider.start();
+
+var aec_slider = new IdealImageSlider.Slider({
+    selector: '#aec_slider',
+    height: 400, // Required but can be set by CSS
+    interval: 5000
+});
+aec_slider.start();
+
+var freistadt_slider = new IdealImageSlider.Slider({
+    selector: '#freistadt_slider',
+    height: 400, // Required but can be set by CSS
+    interval: 5000
+});
+freistadt_slider.start();
